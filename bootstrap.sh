71ecb84da7771217b1970a6a9367a3db6d81fe25
Jeb Bearer <jbearer@hmc.edu>
2016-07-06 21:59:13 -0700

    Add explicit sudos in bootstrap to allow copy/paste

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,13 @@
 # Install git so we can get the rest of this repo
-if ! apt-get install git; then
+if ! sudo apt-get install git; then
     echo "Unable to install git. The config repo cannot be installed."
     exit 1
 fi
 
-if ! git clone https://github.com/jbearer/config.git ~/config; then
+if ! sudo git clone https://github.com/jbearer/config.git ~/config; then
     echo "Unable to clone config repo. Installation unsuccessful."
     exit 1
 fi
 
 cd ~/config
-. install.sh "$@"
+sudo ./install.sh "$@"
