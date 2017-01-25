a8c51dfe6f0cc25b40bd3de51aa3a39564c9b70a
Jeb Bearer <jbearer@hmc.edu>
2017-01-24 16:04:04 -0800

    Add partial install and install from branch options to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,36 @@
+partial=false
+branch=master
+
+while [ "$1" != "--" ] && [ "$#" != "0" ]; do
+    case "$1" in
+        "-p"|"--partial" )
+            partial=true
+            ;;
+        "-b"|"--branch" )
+            if [ "$#" == 1 ]; then
+                echo 2>&1 "--branch requires argument"
+                exit 1
+            fi
+            case "$2" in
+                -* )
+                    echo 2>&1 "--branch requires argument"
+                    exit 1
+                    ;;
+                * )
+                    branch="$2"
+                    shift
+                    ;;
+            esac
+            ;;
+    esac
+
+    shift
+done
+
+if [ "$1" == "--" ]; then
+    shift
+fi
+
 # Install git so we can get the rest of this repo
 if ! sudo apt-get -y install git; then
     echo "Unable to install git. The config repo cannot be installed."
@@ -9,5 +42,9 @@ if ! git clone https://github.com/jbearer/config.git ~/config; then
     exit 1
 fi
 
-cd ~/config
-sudo ./install.sh "$@"
+(cd ~/config; git checkout "$branch")
+
+if ! $partial; then
+    cd ~/config
+    sudo ./install.sh "$@"
+fi
